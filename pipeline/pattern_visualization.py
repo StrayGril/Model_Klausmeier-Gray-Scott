5d717f750d3d0cd4b3d6a09de95a18b647d612ad
Gonieeg <goniee.g@gmail.com>
2026-03-16 22:18:53 +0100

    Generowanie wzorów w modelu bezwymiarowym

--- pipeline/pattern_visualization.py
+++ pipeline/pattern_visualization.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-from numpy.random import default_rng
-
 # Dodanie katalogu głównego repo do ścieżki
 sys.path.append(os.path.abspath(".."))
 
@@ -42,12 +40,13 @@ def initial_conditions(Nx, Ny, a, m, brzeg, noise=1e-3):
 # --------------------------------------------------
 # Symulacja wzorów
 # --------------------------------------------------
-def simulate_patterns(a, m, d1, d2, Lx, Ly, Nx, Ny, T, wykres):
-    # tworzy wszystkie potrzebne macierz
-    # korzysta z ww funkcji
-    # liczy aż powstaną wzory (do kiedy? obecnie T razy)
-    # zwraca dane do wykresu (wykres= "u", "v" lub "uv" lub brak, ze wtedy robi wyrkesy dla u lub v lub obu
-
+def simulate_patterns(a, m, d1, d2, Lx, Ly, Nx, Ny, T):
+    """
+        Przeprowadza symulację o podanych parametrach na T powtórzeń.
+        Parametry:
+            ...
+        Zwraca słownik danych do wykresu, najważniejsze to stany końcowe symulacji: macierze uT i vT.
+    """
     x, y, X, Y, h = make_grid(Lx, Ly, Nx, Ny)
     brzeg = dirichlet_boundary_mask(X, Y, Lx, Ly)
 
@@ -75,67 +74,78 @@ def simulate_patterns(a, m, d1, d2, Lx, Ly, Nx, Ny, T, wykres):
             u_tmid = u_curr.copy()
             v_tmid = v_curr.copy()
 
-    if "v" in wykres:
 
-        fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+    return {"X": X,
+        "Y": Y,
+        "u0": u_0,
+        "v0": v_0,
+        "u1": u_t1,
+        "v1": v_t1,
+        "umid": u_tmid,
+        "vmid": v_tmid,
+        "uT": u_curr,
+        "vT": v_curr}
 
-        dane = [v_0, v_t1, v_tmid, v_curr]
-        tytuly = ["v(t=0)", "v(t=1)", "v(t=T/2)", "v(t=T)"]
 
-        levelsv = np.linspace(
-            min(d.min() for d in dane),
-            max(d.max() for d in dane),
-            50
-        )
 
-        for ax, d, t in zip(axs.flat, dane, tytuly):
-            im = ax.contourf(X, Y, d.reshape(Ny, Nx), levels=levelsv, cmap="viridis")
-            ax.set_title(t)
+# --------------------------------------------------
+# Wykresy z symulacji
+# --------------------------------------------------
+def plot_patterns(sim_data, wykres="uv"):
+    """
+        Rysuje wykresy symulacji w chwilach: 0, 1, T/2, T.
+
+        Parametry:
+        sim_data - wynik funkcji simulate_patterns
+        wykres : string "u", "v", "uv", które wykresy pokazać
+    """
+    X, Y = sim_data["X"], sim_data["Y"]
+    Ny, Nx = X.shape
+
+    states = {
+        "0": (sim_data["u0"], sim_data["v0"]),
+        "1": (sim_data["u1"], sim_data["v1"]),
+        "T/2": (sim_data["umid"], sim_data["vmid"]),
+        "T": (sim_data["uT"], sim_data["vT"])}
+
+    if "u" in wykres: # wykres wody
+
+        fig, axs = plt.subplots(2,2, figsize=(10,8))
+        dane = [states[t][0] for t in states]
+        levels = np.linspace(min(d.min() for d in dane), max(d.max() for d in dane), 50)
+
+        for ax, (t, (u, _)) in zip(axs.flat, states.items()):
+            im = ax.contourf(X, Y, u.reshape(Ny, Nx), levels=levels, cmap="Spectral")
+            ax.set_title(f"u(t={t})")
 
         fig.colorbar(im, ax=axs)
-        #plt.tight_layout()
         plt.show()
 
-    if "u" in wykres:
 
-        fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+    if "v" in wykres: # wykres biomasy
 
-        dane = [u_0, u_t1, u_tmid, u_curr]
-        tytuly = ["u(t=0)", "u(t=1)", "u(t=T/2)", "u(t=T)"]
+        fig, axs = plt.subplots(2,2, figsize=(10,8))
+        dane = [states[t][1] for t in states]
+        levels = np.linspace(min(d.min() for d in dane), max(d.max() for d in dane), 50)
 
-        levelsu = np.linspace(
-            min(d.min() for d in dane),
-            max(d.max() for d in dane),
-            50
-        )
-
-        for ax, d, t in zip(axs.flat, dane, tytuly):
-            im = ax.contourf(X, Y, d.reshape(Ny, Nx), levels=levelsu, cmap="viridis")
-            ax.set_title(t)
+        for ax, (t, (_, v)) in zip(axs.flat, states.items()):
+            im = ax.contourf(X, Y, v.reshape(Ny, Nx), levels=levels, cmap="RdYlGn")
+            ax.set_title(f"v(t={t})")
 
         fig.colorbar(im, ax=axs)
-        #plt.tight_layout()
         plt.show()
 
-    return u_curr, v_curr
 
-
-
-
-# Wykresy z symulacji - robia sie w symualcji obecnie zeby podgladac jej stan bo ona Robi Rzeczy^TM
-def plot_patterns(wynik_simulate_patterns):
-    # robi wykres z wektorów u_fin, v_fin
-    ...
-
-
-# =====================================
-# Próba 1 uzyskania wzorów (wychodzi dziwnie ale sa to jakies wzory?)
+# ===============================================
+# Wychodza wzory!! dla np L, N = 20, 60, T=8000
 
 a, m, d1, d2 = 1, 0.45, 1, 0.02
 
-L, N = 10, 30
+L, N = 20, 60
 
-simulate_patterns(a, m, d1, d2, L, L, N, N, T=500, wykres="v")
+wynik = simulate_patterns(a, m, d1, d2, L, L, N, N, T=8000)
+plot_patterns(wynik, wykres="uv")
 
 
-
+# DALEJ:
+# sprobowac dac w, n zamiast u, v i - czy zadziala dla wielowymiarowego
